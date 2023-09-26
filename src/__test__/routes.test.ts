8dd3bf61ebedf65a0e2f8ee3c14a705742cbd4d5
Marawan-Osama <marawanegy@gmail.com>
2023-09-26 12:34:53 +0300

    added testing comment

--- src/__test__/routes.test.ts
+++ src/__test__/routes.test.ts
@@ -2,6 +2,8 @@ import testRoutes from "../modules/getfortest"
 import request from "supertest";
 import app from "../app";
 
+
+//testing only works without the app.listen in app.ts
 describe('GET /', ()=>{
     it("should return 200 OK and get the json", async ()=>{
         const res = await request(app).get('/').expect({msg: 'hello'});
